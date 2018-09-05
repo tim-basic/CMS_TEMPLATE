637b95317ade4dcb1c47a5e415f56cbfe4e8d63a
tim-basic <t.barbarino@hotmail.com>
2018-09-06 01:57:59 +1000

    commenting

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,25 +1,31 @@
 <div class="col-md-4">
 
     <?php
-        echo "noyo";
+        // isset function to check that 'submit' cell in $_POST is populated
         if (isset($_POST['submit']))
         {
+            // if so, echo whatever is in 'search' cell of $_POST array
             echo $search = $_POST['search'];
         }
-        //echo $search = $_POST['search'];
 
     ?>
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
+        <!-- form action="" directs url
+        method="" sets which arrays are being used -->
         <form action="" method="post">
+            <!-- not sure what this does -->
             <div class="input-group">
+                <!-- input name="" sets name of cell in $_POST[] (post method array)-->
                 <input name="search" type="text" class="form-control">
+                <!-- not sure what span class ="" does -->
                 <span class="input-group-btn">
-                            <button name="submit" class="btn btn-default" type="submit">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
+                    <!-- $_POST['submit] set, boolean YES/NO response set by type="submit" -->
+                    <button name="submit" class="btn btn-default" type="submit">
+                    <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
             </div>
         </form><!--search form-->
         <!-- /.input-group -->
